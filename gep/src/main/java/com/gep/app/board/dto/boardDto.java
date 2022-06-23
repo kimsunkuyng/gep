bea68356791a94f24e0bc811750d2b959cc1d8a0
kimsunkuyng <buxzz88@naver.com>
2022-06-23 17:08:24 +0900

    20220623 first commit

---
+++ gep/src/main/java/com/gep/app/board/dto/boardDto.java
@@ -0,0 +1,96 @@
+package com.gep.app.board.dto;
+
+public class boardDto {
+	private String boardIndex;
+	private String boardCatIndex;
+	private String boardTitle;
+	private String boardContents;
+	private String userIndex;
+	private String boardWriter;
+	private String boardWriterPw;
+	private String boardDate;
+	private String boardPw;
+	private String boardHighlight;
+	private String boardCount;
+	private String boardImg;
+	private String boardCommentIndex;
+	
+	public String getBoardIndex() {
+		return boardIndex;
+	}
+	public void setBoardIndex(String boardIndex) {
+		this.boardIndex = boardIndex;
+	}
+	public String getBoardCatIndex() {
+		return boardCatIndex;
+	}
+	public void setBoardCatIndex(String boardCatIndex) {
+		this.boardCatIndex = boardCatIndex;
+	}
+	public String getBoardTitle() {
+		return boardTitle;
+	}
+	public void setBoardTitle(String boardTitle) {
+		this.boardTitle = boardTitle;
+	}
+	public String getBoardContents() {
+		return boardContents;
+	}
+	public void setBoardContents(String boardContents) {
+		this.boardContents = boardContents;
+	}
+	public String getUserIndex() {
+		return userIndex;
+	}
+	public void setUserIndex(String userIndex) {
+		this.userIndex = userIndex;
+	}
+	public String getBoardWriter() {
+		return boardWriter;
+	}
+	public void setBoardWriter(String boardWriter) {
+		this.boardWriter = boardWriter;
+	}
+	public String getBoardWriterPw() {
+		return boardWriterPw;
+	}
+	public void setBoardWriterPw(String boardWriterPw) {
+		this.boardWriterPw = boardWriterPw;
+	}
+	public String getBoardDate() {
+		return boardDate;
+	}
+	public void setBoardDate(String boardDate) {
+		this.boardDate = boardDate;
+	}
+	public String getBoardPw() {
+		return boardPw;
+	}
+	public void setBoardPw(String boardPw) {
+		this.boardPw = boardPw;
+	}
+	public String getBoardHighlight() {
+		return boardHighlight;
+	}
+	public void setBoardHighlight(String boardHighlight) {
+		this.boardHighlight = boardHighlight;
+	}
+	public String getBoardCount() {
+		return boardCount;
+	}
+	public void setBoardCount(String boardCount) {
+		this.boardCount = boardCount;
+	}
+	public String getBoardImg() {
+		return boardImg;
+	}
+	public void setBoardImg(String boardImg) {
+		this.boardImg = boardImg;
+	}
+	public String getBoardCommentIndex() {
+		return boardCommentIndex;
+	}
+	public void setBoardCommentIndex(String boardCommentIndex) {
+		this.boardCommentIndex = boardCommentIndex;
+	}
+}
